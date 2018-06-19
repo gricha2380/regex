8415aeb91c45534b04b079d80ec1c54a3f434dc8
GRicha2380 <gregor.richardson@gmail.com>
2018-06-18 20:19:30 -0700

    new selection in progress

--- demo/js/battle.js
+++ demo/js/battle.js
@@ -1,37 +1,68 @@
 $(".card").on("click", function(e){
-    // console.log("hi",e)
-    console.log("hi",$(this).attr("value"));
-    $("#hand").attr("value", ($(this).attr("value")))
-    
-    // function(content) {
-    //     console.log("this is content", content)
-    //     return content += $(this).attr("value");
-    // }) 
+    let cardValue = $(this).attr("value");
+    // console.log("hi",$(this).attr("value"));
+    // $("#hand").attr("value", ($(this).attr("value")))
+
+    $('#hand').val(
+        function() { 
+            return $(this).val() ? $(this).val() + `${cardValue}` : cardValue; 
+        }
+    );
+})
+
+$(".clear").on("click", function(e){
+    $("#hand").val("");
 })
 
 document.querySelector("#attack").addEventListener("click", e=>{
     battle();    
 })
-
+//current problem: when full string is returned inside enemyMatch it's all stored in index 0.
+// possible solution: conditional for if enemyMatach.length = 0;
 let battle = ()=> {
     let enemies = document.querySelector('#enemies').innerText;
-    console.log("enemies value",enemies)
-    let newArray = enemies.split("")
-    console.log(newArray)
+    console.log("enemies value",enemies);
+    let enemyArray = enemies.split(""); // ["a", "b", "e", "1", "3", "3"]
+    console.log("enemyArray content",enemyArray);
     // let playerPattern = /\d/g; // static value
     let playerPattern = new RegExp(document.querySelector("#hand").value,"g");
-    let results = enemies.match(playerPattern)
-    console.log("results",results);
+    console.log("playerpattern",playerPattern); //  /\d/g
+    let enemyMatch = enemies.match(playerPattern);
+    console.log("enemyMatch",enemyMatch); //  ["1", "3", "3"]
     enemies = '';
-    newArray.forEach((element,x) => {
-        for(let i = 0;i<newArray.length;i++){
-            if (element==results[i]) {
-                console.log(`match ${element}`)
-                newArray[x] = `<b>${element}</b>`
+    if (enemyMatch.length === 1) {
+        enemyArray.forEach((element,x) => {
+            console.log("element char", element)
+            
+            for(let i = 0;i<enemyArray.length;i++){
+                console.log("itterator",i)
+                if (enemyMatch[i]) console.log(enemyMatch[i]) 
+                // console.log("enemyMatch char", enemyMatch[i])
+                if (enemyMatch[0][i] == enemyArray[element]) {
+                    console.log(`match ${element}`);
+                    enemyArray[x] = `<b>${element}</b>`;
+                }
             }
-        }
-        enemies += newArray[x];
-    });
-    console.log("all of it",newArray)
+            enemies += enemyArray[x];
+        });
+
+    }
+    else {
+        enemyArray.forEach((element,x) => {
+            console.log("element char", element)
+            
+            for(let i = 0;i<enemyArray.length;i++){
+                console.log("itterator",i)
+                if (enemyMatch[i]) console.log(enemyMatch[i]) 
+                // console.log("enemyMatch char", enemyMatch[i])
+                if (enemyMatch[i] && enemyArray[element]==enemyMatch[i]) {
+                    console.log(`match ${element}`);
+                    enemyArray[x] = `<b>${element}</b>`;
+                }
+            }
+            enemies += enemyArray[x];
+        });
+    }
+    console.log("all of it",enemyArray);
     document.querySelector('#enemies').innerHTML = enemies;
 }    
