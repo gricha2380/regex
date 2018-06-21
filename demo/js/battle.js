1c002fa463c7494c304734f6a44d5e1dc8bb8e4a
GRicha2380 <gregor.richardson@gmail.com>
2018-06-20 20:36:44 -0700

    basic model config

--- demo/js/battle.js
+++ demo/js/battle.js
@@ -26,17 +26,16 @@ document.querySelector("#attack").addEventListener("click", e=>{
 let battle = ()=> {
     let enemies = document.querySelector('#enemies').innerText;
     console.log("enemies value",enemies);
-    let enemyArray = enemies.split(""); // ["a", "b", "e", "1", "3", "3"]
+    let enemyArray = enemies.split(""); // e.g.:["a", "b", "e", "1", "3", "3"]
     console.log("enemyArray content",enemyArray);
-    // let playerPattern = /\d/g; // static value
-    let playerPattern = new RegExp(document.querySelector("#hand").value,"g");
-    console.log("playerpattern",playerPattern); //  /\d/g
+    let playerPattern = new RegExp($("#hand").val(),"g"); //let playerPattern = /\d/g; // static test
+    console.log("playerpattern",playerPattern); //  e.g.:/\d/g
     let enemyMatch = enemies.match(playerPattern);
     if (!enemyMatch) {
         alert.innerText = "No matches...";
         return;
     }
-    console.log("enemyMatch",enemyMatch); //  ["1", "3", "3"]
+    console.log("enemyMatch",enemyMatch); // e.g.:["1", "3", "3"]
     enemies = '';
     if (enemyMatch.length === 1) {
         console.log("enemyMatch length",enemyMatch[0].length)
@@ -53,6 +52,6 @@ let battle = ()=> {
     else {
        console.log("enemy match longer than 1")
     }
-    console.log("all of it",enemyArray);
+    console.log("all of enemyArray",enemyArray);
     document.querySelector('#enemies').innerHTML = enemies;
 }    
